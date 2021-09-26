1731ea282a311d5f144e0f411e6e242808d5f9f9
Dante Faña Badia <dfana@dfb.com.do>
2021-09-26 17:29:27 -0400

    Change model

--- lambda/buildmodel.js
+++ lambda/buildmodel.js
@@ -6,14 +6,4 @@ new ControlInteractionModelGenerator()
     .addIntent({ name: "AMAZON.NavigateHomeIntent" })
     .addIntent({ name: "AMAZON.HelpIntent" })
     .addIntent({ name: "AMAZON.CancelItem" })
-    .addIntent({ name: "ShowProductsIntent", samples: [
-        "show me products",
-        "list me products",
-        "show products",
-        "list products",
-        "show product",
-        "list product",
-        "show me all the products",
-        "show me all products",
-    ]}) 
     .buildAndWrite('../skill-package/interactionModels/custom/en-US.json');
